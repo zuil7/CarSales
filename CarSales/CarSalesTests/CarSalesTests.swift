fad934141f7d5f76ca5fd16c5a3793daae66adbe
Louise Namoc <zuilse7en@gmail.com>
2021-08-12 15:43:12 +0800

    'Initial commit'

---
+++ CarSales/CarSalesTests/CarSalesTests.swift
@@ -0,0 +1,34 @@
+//
+//  CarSalesTests.swift
+//  CarSalesTests
+//
+//  Created by Louise Nicolas Namoc.
+//  Copyright © 2020 "Organization Name". All rights reserved.
+//
+
+import XCTest
+@testable import CarSales
+
+class CarSalesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
